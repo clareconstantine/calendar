66a9fb79dfbf9259fb5a0e7fc529006a6fd06583
Clare Constantine <clare.constantine@gmail.com>
2016-01-25 23:04:26 -0800

    Create Event objects from input array. Display event info

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,9 +1,10 @@
 class CalendarController < ApplicationController
   def index
-  	# todo: eventually get events somewhere else?
   	@events = [
-		{ name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
-		{ name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30' }
-	]
+  		{ name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
+  		{ name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30' }
+  	]
+
+    @events.map! { |e| Event.new(e) }
   end
 end
