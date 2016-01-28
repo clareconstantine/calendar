b38f85e406065370b7b5658cb462b15e91bc610c
Clare Constantine <clare.constantine@gmail.com>
2016-01-27 22:18:37 -0800

    overlapping events and css tweaks

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,10 +3,14 @@ class CalendarController < ApplicationController
     #start and end times will be assumed to be am unless otherwise specified (ie, '12:30pm')
   	@events = [
   		{ name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
+      { name: 'Climbing', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
+      { name: 'Ice cream', day: 'Sunday', start_time: '8:30', end_time: '9:00' },
   		{ name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30pm' },
-      { name: 'Ice cream', day: 'Tuesday', start_time: '11:30pm', end_time: '1:00pm' },
-      { name: 'Ice cream', day: 'Friday', start_time: '12:00pm', end_time: '1:00pm' },
-      { name: 'Ice cream', day: 'Sunday', start_time: '8:30', end_time: '9:00' }
+      { name: 'Lunch', day: 'Monday', start_time: '12:00pm', end_time: '1:00pm' },
+      { name: 'Birthday!', day: 'Tuesday', start_time: '11:30pm', end_time: '1:30pm' },
+      { name: 'Lunch', day: 'Friday', start_time: '11:00', end_time: '1:00pm' },
+      { name: 'Parade', day: 'Friday', start_time: '11:30', end_time: '12:30pm' }
+
 
   	]
 
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -5,7 +5,7 @@ module CalendarHelper
   TIMES = ['12:00am', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am','6:00am',
     '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm']
 
-  # these are offset from TIMES_TO_ABSOLUTE in the Event model because the 12:00am label
+  # these are offset from times_to_absolute in the Event model because the 12:00am label
   # is in the cell row that corresponds 10 11:30pm (it is labeling its bottom border)
   TIME_LABELS_TO_ABSOLUTE = { '12:00am' => 0,
                               '1:00am' => 2,
@@ -30,7 +30,7 @@ module CalendarHelper
   def day_labels
     labels = ""
     DAYS.each do |d|
-      labels << content_tag(:th, d, :class => 'day')
+      labels << content_tag(:th, d, { :class => 'day', :colspan => '2' })
     end
     labels.html_safe
   end
@@ -47,7 +47,7 @@ module CalendarHelper
   def row absolute_value, time=nil
     label = time ? time : ""
     row_class = time ? 'halfhour' : 'hour'
-    cells = content_tag(:td, label, :class => 'time')
+    cells = content_tag(:td, label, { :class => 'time', :colspan => '2' })
     cells << daily_cells(absolute_value)
 
     content_tag(:tr, cells, :class => row_class)
@@ -56,14 +56,44 @@ module CalendarHelper
   def daily_cells absolute_value
     cells = ""
     DAYS.each do |d|
-      # todo: this logic relies on events not overlapping
-      event_starting = event_start(absolute_value, d)
-      event_continuing = event_continuation(absolute_value, d)
+      events_starting = event_start(absolute_value, d)
+      events_continuing = event_continuation(absolute_value, d)
 
-      if event_starting
-        cells << content_tag(:td, event_starting.name, { :class => 'event', :rowspan => event_starting.duration.to_s} )
-      elsif !event_continuing # if an even is continuing we don't want to make a new cell
-        cells << content_tag(:td)        
+      # two events start simultaneously
+      if events_starting.size == 2
+        events_starting.each do |e|
+          cells << content_tag(:td, e.name, { :class => 'event', :rowspan => e.duration.to_s, :colspan => '1' })
+        end
+      # one event starts, one is continuing
+      elsif events_starting.size == 1 && events_continuing.size == 1
+        e = events_starting.first
+        cells << content_tag(:td, e.name, { :class => 'event', :rowspan => e.duration.to_s, :colspan => '1' })
+      # one event starts, none continuing
+      elsif events_starting.size == 1
+        e = events_starting.first
+        events_overlapping = event_overlap e
+
+        # if no overlapping events
+        if events_overlapping.size == 0
+          cells << content_tag(:td, e.name, { :class => 'event', :rowspan => e.duration.to_s, :colspan => '2' })
+        # overlapping event starting later
+        else
+          cells << content_tag(:td, "", :colspan => '1')
+          cells << content_tag(:td, e.name, { :class => 'event', :rowspan => e.duration.to_s, :colspan => '1' })
+        end
+      # one continuing event
+      elsif events_continuing.size == 1
+        e = events_continuing.first
+        events_overlapping = event_overlap e
+
+        # do nothing if the event is continuing and does not overlap anything
+        # but if it overlaps an event...
+        if events_overlapping.size == 1
+          cells << content_tag(:td, "", :colspan => '1')
+        end
+      # no events starting or continuing
+      elsif events_continuing.size == 0
+        cells << content_tag(:td, "", :colspan => '2')
       end    
     end
 
@@ -71,13 +101,27 @@ module CalendarHelper
   end
 
   def event_start absolute_value, day
-    events = @events.select {|e| e.day == day && e.start_time == absolute_value}
-    events.first
+    @events.select {|e| e.day == day && e.start_time == absolute_value}
   end
 
   def event_continuation absolute_value, day
-    events = @events.select {|e| e.day == day && e.start_time < absolute_value && e.end_time > absolute_value}
-    events.first
+    @events.select {|e| e.day == day && e.start_time < absolute_value && e.end_time > absolute_value}
+  end
+
+  def event_overlap cur_event
+    events = @events.select {|e| overlap?(e, cur_event)}
+  end
+
+  def overlap? e1, e2
+    overlap = false
+    if e1.day == e2.day
+      if (e1.start_time > e2.start_time) && (e1.start_time < e2.end_time)
+        overlap = true
+      elsif (e1.end_time > e2.start_time) && (e1.end_time < e2.end_time)
+        overlap = true
+      end
+    end
+    overlap
   end
 
 end
