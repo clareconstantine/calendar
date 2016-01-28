8ab92d28526d4d6d6ce2510e7753d1eaeee51485
Clare Constantine <clare.constantine@gmail.com>
2016-01-27 20:10:38 -0800

    events get solid border

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,7 +3,11 @@ class CalendarController < ApplicationController
     #start and end times will be assumed to be am unless otherwise specified (ie, '12:30pm')
   	@events = [
   		{ name: 'Yoga', day: 'Sunday', start_time: '9:00', end_time: '10:00' },
-  		{ name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30pm' }
+  		{ name: 'Running', day: 'Monday', start_time: '11:00', end_time: '12:30pm' },
+      { name: 'Ice cream', day: 'Tuesday', start_time: '11:30pm', end_time: '1:00pm' },
+      { name: 'Ice cream', day: 'Friday', start_time: '12:00pm', end_time: '1:00pm' },
+      { name: 'Ice cream', day: 'Sunday', start_time: '8:30', end_time: '9:00' }
+
   	]
 
     @events.map! { |e| Event.new(e) }
